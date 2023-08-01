dc2720330b216f6defcdd5b6a23f7617ead1f175
EstherDelap <py20ed@leeds.ac.uk>
2023-08-01 14:38:26 +0100

    testing out the testing .4

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,10 @@ using DTWA
 
 @test DTWA.greet("hello") === "hello"
 
-@test size(DTWA.Interactions(3,3,3).data)===(3,3,3,3,3,3)
-
 A = DTWA.Interactions(3,3,3)
 
+@test size(A.data)===(3,3,3,3,3,3)
+
 @test DTWA.dims(A) === (3,3,3)
+
+@test DTWA.norm(DTWA.CartesianIndices(A)[2]) === sqrt(6)
