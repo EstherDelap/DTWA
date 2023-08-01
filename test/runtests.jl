16f04645242e17d9575d0e600d829795e4c1482e
EstherDelap <py20ed@leeds.ac.uk>
2023-08-01 14:17:38 +0100

    testing out the testing .3

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,3 +2,9 @@ using Test
 using DTWA
 
 @test DTWA.greet("hello") === "hello"
+
+@test size(DTWA.Interactions(3,3,3).data)===(3,3,3,3,3,3)
+
+A = DTWA.Interactions(3,3,3)
+
+@test DTWA.dims(A) === (3,3,3)
