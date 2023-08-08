ecdb11c4f4d589b5216669a70463bc69c9faafde
Documenter.jl <documenter@juliadocs.github.io>
2023-08-08 11:27:45 +0000

    build based on c549c28

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DTWA.jl","page":"DTWA.jl","title":"DTWA.jl","text":"","category":"section"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"Documentation for DTWA.jl","category":"page"}]
+}
