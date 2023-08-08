d79f573b8ca70a5f2895a741d2179dccf9cf8655
Documenter.jl <documenter@juliadocs.github.io>
2023-08-08 14:55:31 +0000

    build based on b6d648b

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#DTWA.jl","page":"DTWA.jl","title":"DTWA.jl","text":"","category":"section"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"Documentation for DTWA.jl","category":"page"}]
+[{"location":"#DTWA.jl","page":"DTWA.jl","title":"DTWA.jl","text":"","category":"section"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"Documentation for DTWA.jl","category":"page"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"repeated_euler(dim::Vector{Int64}, N::Int64,number_repeats::Int64, Γ_deph::Float64, Γ_decay::Float64,Ω::Float64, α::AbstractVector, method::String, axis::Int64, dir::Int64)","category":"page"}]
 }
