2c85ca8f606ef703fecc244637e3e975510502a8
Documenter.jl <documenter@juliadocs.github.io>
2023-08-08 15:10:28 +0000

    build based on 1bd22ee

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#DTWA.jl","page":"DTWA.jl","title":"DTWA.jl","text":"","category":"section"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"Documentation for DTWA.jl","category":"page"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"repeated_euler(dim, N,number_repeats,Γ_deph, Γ_decay,Ω, α, method, axis, dir)","category":"page"}]
+[{"location":"#DTWA.jl","page":"DTWA.jl","title":"DTWA.jl","text":"","category":"section"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"Documentation for DTWA.jl","category":"page"},{"location":"","page":"DTWA.jl","title":"DTWA.jl","text":"repeated_euler\neuler_3D\nspin_array_3D\nintial_Si\nJ_XYZ\nJx_Ising","category":"page"}]
 }
